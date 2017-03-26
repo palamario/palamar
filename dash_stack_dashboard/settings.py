ce766ca102a5045fd05a8c207d326e3656b1a498
Paarhati Ozkasgarli <ozkasgarli@gmail.com>
2017-03-26 19:04:18 +0300

    Remove django_cleanup
    Removing django_cleanup module. It does not needed anymore.
    
    Change-Id: I937bfb07f4d74f249c814693589f775656e9d4bc

--- dash_stack_dashboard/settings.py
+++ dash_stack_dashboard/settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_cleanup',
     'app',
     'authcp',
     'admincp',
