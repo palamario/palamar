046e549c4426cf7844a41d5d6f15ac10ef373d89
Paarhati Ozkasgarli <ozkasgarli@gmail.com>
2017-03-30 23:42:54 +0300

    Crud Operations for Sites
    Crun for Sites
    
    Change-Id: I32052615011f481868bb9cb48f16f583efedd3eb

--- sites/forms.py
+++ sites/forms.py
@@ -1,26 +1,61 @@
 from oslo_i18n import translate as _
 
 from django import forms
+from . models import Sites
 
-from .models import Sites
 
+class SiteCreateForm(forms.Form):
+    name=forms.CharField(
+        label=_('Name'),
+        max_length=64,
+        required=True
+    )
+    url=forms.CharField(
+        label=_('URL'),
+        max_length=512,
+        required=True
+    )
+    client_cert=forms.FileField(
+        label=_('Client Certificate'),
+        required=True
+    )
+    client_key=forms.FileField(
+        label=_('Client Key'),
+        required=True
+    )
+    ca_cert=forms.FileField(
+        label=_('CA Certificate'),
+        required=True
+    )
+    ssl_verify=forms.BooleanField(
+        label=_('Verify SSL'),
+        required=False
+    )
 
 class SiteEditForm(forms.Form):
     name=forms.CharField(
         label=_('Name'),
-        max_length=64
+        max_length=64,
+        required=True
     )
     url=forms.CharField(
         label=_('URL'),
-        max_length=512
+        max_length=512,
+        required=True
     )
     client_cert=forms.FileField(
-        label=_('Client Certificate')
+        label=_('Client Certificate'),
+        required=False
     )
-    client_key=forms.FileField()
-    ca_cert=forms.FileField()
-    ssl_verify=forms.BooleanField()
-
-    class Meta:
-        model=Sites
-        fields=('name', 'url', 'client_cert', 'client_key', 'ca_cert', 'ssl_verify')
+    client_key=forms.FileField(
+        label=_('Client Key'),
+        required=False
+    )
+    ca_cert=forms.FileField(
+        label=_('CA Certificate'),
+        required=False
+    )
+    ssl_verify=forms.BooleanField(
+        label=_('Verify SSL'),
+        required=False
+    )
--- sites/views.py
+++ sites/views.py
@@ -2,13 +2,42 @@ from oslo_i18n import translate as _
 
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from . models import Sites
-from .forms import SiteEditForm
+from .forms import SiteCreateForm,SiteEditForm
 
 @login_required
 def index(request):
     return render(request, "sites/index.html", {})
+
+@login_required
+def site_create(request):
+    title = _('New Site')
+    subtitle = _('create new site')
+    if request.method=='POST':
+        form=SiteCreateForm(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            site=Sites(
+                name=form.cleaned_data['name'],
+                url=form.cleaned_data['url'],
+                client_cert=form.cleaned_data['client_cert'],
+                client_key=form.cleaned_data['client_key'],
+                ca_cert=form.cleaned_data['ca_cert'],
+                ssl_verify=form.cleaned_data['ssl_verify'],
+            )
+            site.save()
+            messages.success(request,_('New site has been created!'))
+            return redirect('sites-list')
+        else:
+            print(form.errors)
+    else:
+        form=SiteCreateForm()
+    return render(request, "sites/site_create.html", {
+        "title": title,
+        "subtitle": subtitle,
+        "form": form,
+        })
 
 @login_required
 def sites_list(request):
@@ -27,13 +56,25 @@ def site_edit(request,pk):
     title=_('Edit Site')
     subtitle=_('update details of a site')
     breadcrumb=_('Edit Site')
-    site = get_object_or_404(Sites, pk=pk)
+    site=get_object_or_404(Sites, pk=pk)
     if request.method=='POST':
-        form=SiteEditForm(request.POST,request.FILES)
+        form=SiteEditForm(request.POST or None, request.FILES or None)
         if form.is_valid():
-            site.name=form.data['name']
+            site.name=form.cleaned_data['name']
+            site.url=form.cleaned_data['url']
+            if form.cleaned_data['client_cert']:
+                site.client_cert.delete()
+                site.client_cert=form.cleaned_data['client_cert']
+            if form.cleaned_data['client_key']:
+                site.client_key.delete()
+                site.client_key=form.cleaned_data['client_key']
+            if form.cleaned_data['ca_cert']:
+                site.ca_cert.delete()
+                site.ca_cert=form.cleaned_data['ca_cert']
+            site.ssl_verify=form.cleaned_data['ssl_verify']
             site.save()
-            return redirect('sites_edit', pk=pk)
+            messages.success(request, _('Site updated!'))
+            return redirect("sites-list")
         else:
             print(form.errors)
     else:
@@ -45,3 +86,13 @@ def site_edit(request,pk):
         "subtitle": subtitle,
         "form": form,
         })
+
+@login_required
+def site_delete(request,pk):
+    title = _('Delete Site')
+    subtitle = _('are you sure to delete')
+    breadcrumb = _('Delete Site')
+    site=get_object_or_404(Sites,pk=pk)
+    site.delete()
+    messages.success(request, _('Site deleted!'))
+    return redirect("sites-list")
